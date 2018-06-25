205fe0aab61f1c7e1d32ac9003478fafb68755a1
Jim Pick <jim@backblend.com>
2018-06-24 22:23:06 -0700

    Add scripts to rebuild prebuilt native modules

---
+++ README.md
@@ -0,0 +1,11 @@
+# sodium-native-prebuilds-nodejs-mobile
+
+Contains prebuilt native libraries for use with nodejs-mobile
+
+## Development
+
+Use `npm run build` to regenerate prebuilt binaries in prebuilds directory.
+
+## License
+
+MIT
---
+++ build-ios-arm64.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+set -e
+
+topDir=`cd $(dirname $0); pwd`
+NODEJS_HEADERS_DIR=$topDir/node_modules/nodejs-mobile-react-native/ios/libnode
+NODEJS_MOBILE_GYP_BIN_FILE=$topDir/node_modules/nodejs-mobile-gyp/bin/node-gyp.js
+
+export GYP_DEFINES="OS=ios"
+export npm_config_nodedir="$NODEJS_HEADERS_DIR"
+export npm_config_node_gyp="$NODEJS_MOBILE_GYP_BIN_FILE"
+export npm_config_platform="ios"
+export npm_config_node_engine="chakracore"
+export npm_config_arch="arm64"
+export PREBUILD_ARCH=arm64
+export PREBUILD_PLATFORM=ios
+export PREBUILD_NODE_GYP="$NODEJS_MOBILE_GYP_BIN_FILE"
+
+if [ ! -d sodium-native ]; then
+  #git clone git@github.com:sodium-friends/sodium-native.git
+  git clone git@github.com:jimpick/sodium-native.git
+fi
+
+pushd sodium-native
+
+git checkout ios-prebuild
+
+SODIUM_NATIVE=1 npm install
+
+npm run fetch-libsodium
+
+
+pushd node_modules
+rm -rf prebuildify
+git clone git@github.com:jimpick/prebuildify.git
+pushd prebuildify
+git checkout ios
+popd
+popd
+
+npx prebuildify \
+  --strip \
+  --preinstall "node preinstall.js" \
+  --postinstall "node postinstall.js" \
+  --platform=ios --arch=arm64 \
+  --target=node@8.0.0
+
+popd
+
+tar cf - -C sodium-native prebuilds | tar xvf -
